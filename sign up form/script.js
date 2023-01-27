921a6610b2b4e8826ab05227d93e547654812674
ami87878 <111053228+ami87878@users.noreply.github.com>
2023-01-27 16:48:15 +0330

    Add files via upload

---
+++ sign up form/script.js
@@ -0,0 +1,8 @@
+const container=document.querySelector('.container');
+const sigupBtn=document.querySelector('.green-bg button');
+sigupBtn.addEventListener('click',()=>{
+
+container.classList.toggle('change');
+
+
+});
